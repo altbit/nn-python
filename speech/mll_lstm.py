bffeaad16784e6770d9d6521484ef274d48bad7b
Denis Degtyarev <alt.degtyarev@gmail.com>
2017-03-06 21:30:00 +0000

    runable mll lstm

--- speech/mll_lstm.py
+++ speech/mll_lstm.py
@@ -216,12 +216,12 @@ def run_epoch(session, model, eval_op=None, verbose=False):
 
     for step in range(model.epoch_size):
         _inputs, _labels = model.input.get_batch(model.batch_size)
-        inputs = tf.convert_to_tensor(_inputs, name="inputs", dtype=data_type())
-        labels = tf.convert_to_tensor(_labels, name="labels", dtype=data_type())
+        # inputs = tf.convert_to_tensor(_inputs, name="inputs", dtype=data_type())
+        # labels = tf.convert_to_tensor(_labels, name="labels", dtype=data_type())
 
         vals = session.run(fetches, feed_dict={
-            model.inputs_ph: inputs,
-            model.labels_ph: labels
+            model.inputs_ph: _inputs,
+            model.labels_ph: _labels
         })
         # state = vals["final_state"]
         costs += vals["cost"]
